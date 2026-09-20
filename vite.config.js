741f94a83d98cf8226a4cac066211b0680b5aab0
Mohammed Ali <mohammedalijaafar1999@gmail.com>
2026-09-20 20:02:08 +0300

    Migrate from Parcel 1 to Vite 5 (Node 26 compatible)

--- vite.config.js
+++ vite.config.js
@@ -1,134 +1,39 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
-const { resolve } = require("path");
+import { resolve } from "path";
 
+// Serve /css/* and /javascript/* from src/ in dev (HTML references them via ../)
+const serveSrcStatic = {
+  name: "serve-src-static",
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      if (req.url && req.url.startsWith("/css/")) {
+        req.url = "/@fs/" + resolve(__dirname, "src/css").replace(/\\/g, "/") + req.url.slice(4);
+      } else if (req.url && req.url.startsWith("/javascript/")) {
+        req.url = "/@fs/" + resolve(__dirname, "src/javascript").replace(/\\/g, "/") + req.url.slice(11);
+      }
+      next();
+    });
+  },
+};
+
+// Vite config for the portfolio.
+// - Root is src/pages so index.html and Work.html are served directly.
+// - /public is served at "/" (Vite default), so images live at /Images/...
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      entry: "main.js",
-      inject: {
-        data: {
-          nav: `<nav class="menu">
-          <div class="menu__item">
-            <a href="/" class="menu__item-link">Home</a>
-            <div class="marquee">
-              <div class="marquee__inner-wrap">
-                <div class="marquee__inner" aria-hidden="true">
-                  <span>Frank Tower</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Dom Dom</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Santa Maria</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Big Molly</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <!-- <span>Frank Tower</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Dom Dom</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Santa Maria</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Big Molly</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div> -->
-                </div>
-                <!--/marquee__inner-->
-              </div>
-              <!--/marquee__inner-wrap-->
-            </div>
-            <!--/marquee-->
-          </div>
-          <!--/menu__item-->
-          <div class="menu__item">
-            <a href="/Work.html" class="menu__item-link">Work</a>
-            <div class="marquee">
-              <div class="marquee__inner-wrap">
-                <div class="marquee__inner" aria-hidden="true">
-                  <span>Frank Tower</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Dom Dom</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Santa Maria</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Big Molly</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <!-- <span>Frank Tower</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Dom Dom</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Santa Maria</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div>
-                  <span>Big Molly</span>
-                  <div
-                    class="marquee__img"
-                    style="background-image: url(https://picsum.photos/300/200)"
-                  ></div> -->
-                </div>
-                <!--/marquee__inner-->
-              </div>
-              <!--/marquee__inner-wrap-->
-            </div>
-            <!--/marquee-->
-          </div>
-          <!--/menu__item-->
-          <!-- ... -->
-        </nav>
-        <!--/menu-->`,
-        },
-      },
-    }),
-  ],
+  root: "src/pages",
+  publicDir: resolve(__dirname, "public"),
+  plugins: [serveSrcStatic],
   build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        admin: resolve(__dirname, "admin/index.html"),
+        main: resolve(__dirname, "src/pages/index.html"),
+        work: resolve(__dirname, "src/pages/Work.html"),
       },
     },
+  },
+  server: {
+    open: true,
   },
 });
